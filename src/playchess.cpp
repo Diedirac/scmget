7bffcd9e98b46d088b32fda77fc48cc2a9384085
Jackson Fu <diedirac@gmail.com>
2021-06-02 15:58:02 +0800

    Add files via upload
    c

--- src/playchess.cpp
+++ src/playchess.cpp
@@ -11,122 +11,6 @@
 
 using namespace std;
 
-const int TIE_THRESHOLD = 50;
-
-void startAIAdversarialChess(int level, FILE* fp, unsigned long long* nodeCount) {
-	ChessBoard board;
-	list<Move> regulars, nulls;
-	int turn = WHITE;
-	Move move;
-	bool found;
-	
-	double start, decisionTime;
-
-	// Initialize players
-	AIPlayer black(BLACK, level);
-	AIPlayer white(WHITE, level);
-
-	// setup board
-	board.initDefaultSetup();
-	char mesg[30];
-	int turnCount = 0;
-	int blackRepeatNum = 0;
-	Move blackLastMove;
-	int whiteRepeatNum = 0;
-	Move whiteLastMove;
-	
-	for(;;) {
-		// show board
-		//board.print();
-		*nodeCount = 0;
-
-		// query player's choice
-		if(turn) {
-			fprintf(fp, "%d,black,", turnCount);
-			
-			start = CycleTimer::currentSeconds();
-			found = black.getMove(board, move, nodeCount);
-			decisionTime = CycleTimer::currentSeconds() - start;
-			fprintf(fp, "%llu,%.20f,", *nodeCount, decisionTime);
-			
-			if(turnCount == 1)
-				blackLastMove = move;
-			else {
-				if(FIGURE(move.figure) == FIGURE(blackLastMove.figure))
-					blackRepeatNum++;
-				else
-					blackRepeatNum = 0;
-				
-				blackLastMove = move;
-			}
-		}
-		else {
-			fprintf(fp, "%d,white,", turnCount);
-			start = CycleTimer::currentSeconds();
-			found = white.getMove(board, move, nodeCount);
-			decisionTime = CycleTimer::currentSeconds() - start;
-			fprintf(fp, "%llu,%.20f,", *nodeCount, decisionTime);
-			
-			if(turnCount == 0)
-				whiteLastMove = move;
-			else {
-				if(FIGURE(move.figure) == FIGURE(whiteLastMove.figure))
-					whiteRepeatNum++;
-				else 
-					whiteRepeatNum = 0;
-				
-				whiteLastMove = move;
-			}
-		}
-		
-		if(found) {
-			move.print(mesg);
-			fprintf(fp, "%s,\n", mesg);
-		}
-		
-		if(whiteRepeatNum > TIE_THRESHOLD || blackRepeatNum > TIE_THRESHOLD) {
-			printf("Repeat move the same piece %d times./nThis game ended in a tie.\n", TIE_THRESHOLD);
-			break;
-		}
-
-		if(!found)
-			break;
-
-		// if player has a move get all moves
-		regulars.clear();
-		nulls.clear();
-		board.getMoves(turn, regulars, regulars, nulls);
-
-		// execute maintenance moves
-		for(list<Move>::iterator it = nulls.begin(); it != nulls.end(); ++it)
-			board.move(*it);
-
-		// execute move
-		board.move(move);
-		//move.print(mesg);
-
-		// opponents turn
-		turn = TOGGLE_COLOR(turn);
-		turnCount++;
-	}
-	
-	ChessPlayer::Status status = board.getPlayerStatus(turn);
-
-	switch(status)
-	{
-		case ChessPlayer::Checkmate:
-			printf("Checkmate\n");
-			fprintf(fp, "Checkmate,\n");
-			break;
-		case ChessPlayer::Stalemate:
-			printf("Stalemate\n");
-			fprintf(fp, "Stalemate,\n");
-			break;
-	}
-	
-	board.print();
-}
-
 void gmInput(ChessBoard* board, HumanPlayer * p, char* input, Move * move, int turn) {
 	list<Move> regulars, nulls;
 
@@ -157,6 +41,10 @@ int main() {
 	int turn = WHITE;
 	Move move;
 	double start, decisionTime;
+	FILE *fp;
+	char buffer[100];
+
+
 	char* pos = (char *) malloc(5 * sizeof(char));
 
 	// Initialize players
@@ -165,75 +53,32 @@ int main() {
 
 	// setup board
 	board.initDefaultSetup();
-	strcpy(pos, "b2b4");
-
-	gmInput(&board, &p1, pos, &move, turn);
+	fscanf(fp, "%s\n", buffer);
 	board.print();
-
-	free(pos);
 	
-	/*for(;;) {
+	if(!(fp = fopen("Game4_LV3_Record.csv", "r")))
+	{
+		printf("File open failed!\n");
+		return -1;
+	}
+
+	for(int i = 0; i < 1; ++i) {
 		// show board
-		//board.print();
-		*nodeCount = 0;
+		fscanf(fp, "%s,%s,\n", NULL, buffer);
+		
 
 		// query player's choice
 		if(turn) {
-			fprintf(fp, "%d,black,", turnCount);
-			
-			start = CycleTimer::currentSeconds();
-			found = black.getMove(board, move, nodeCount);
-			decisionTime = CycleTimer::currentSeconds() - start;
-			fprintf(fp, "%llu,%.20f,", *nodeCount, decisionTime);
+			gmInput(&board, &p2, pos, &move, turn)
 		}
 		else {
-			fprintf(fp, "%d,white,", turnCount);
-			start = CycleTimer::currentSeconds();
-			found = white.getMove(board, move, nodeCount);
-			decisionTime = CycleTimer::currentSeconds() - start;
-			fprintf(fp, "%llu,%.20f,", *nodeCount, decisionTime);
-			
-			if(turnCount == 0)
-				whiteLastMove = move;
-			else {
-				if(FIGURE(move.figure) == FIGURE(whiteLastMove.figure))
-					whiteRepeatNum++;
-				else 
-					whiteRepeatNum = 0;
-				
-				whiteLastMove = move;
-			}
+			gmInput(&board, &p1, pos, &move, turn)
 		}
-		
-		if(found) {
-			move.print(mesg);
-			fprintf(fp, "%s,\n", mesg);
-		}
-		
-		if(whiteRepeatNum > TIE_THRESHOLD || blackRepeatNum > TIE_THRESHOLD) {
-			printf("Repeat move the same piece %d times./nThis game ended in a tie.\n", TIE_THRESHOLD);
-			break;
-		}
-
-		if(!found)
-			break;
-
-		// if player has a move get all moves
-		regulars.clear();
-		nulls.clear();
-		board.getMoves(turn, regulars, regulars, nulls);
-
-		// execute maintenance moves
-		for(list<Move>::iterator it = nulls.begin(); it != nulls.end(); ++it)
-			board.move(*it);
-
-		// execute move
-		board.move(move);
-		//move.print(mesg);
-
+	
 		// opponents turn
 		turn = TOGGLE_COLOR(turn);
-	}*/
+		board.print();
+	}
 	
 	return 0;
 }
